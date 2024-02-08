6fe9af55d6da70cdbd3632d2dc539b00d1bce9ae
Go Anon <159459403+go-anon@users.noreply.github.com>
2024-02-08 22:27:38 +0000

    implementing types

---
+++ types/ints/ints.go
@@ -0,0 +1,17 @@
+package ints
+
+import "strconv"
+
+func Get(str string) int {
+
+	v, _ := strconv.Atoi(str)
+
+	return v
+}
+
+func Get64(str string) int64 {
+
+	v, _ := strconv.ParseInt(str, 10, 64)
+
+	return v
+}
